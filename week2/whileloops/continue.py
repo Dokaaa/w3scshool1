7fc3db4bc8410d9056c8a89cd74787625e8e880e
Dokaaa <122877634+Dokaaa@users.noreply.github.com>
2023-01-29 03:10:27 +0600

    Add files via upload

---
+++ week2/whileloops/continue.py
@@ -0,0 +1,7 @@
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+# number 3 is missing in output
---
+++ week2/whileloops/else.py
@@ -0,0 +1,7 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+#run a block of code once when the condition no longer is true
