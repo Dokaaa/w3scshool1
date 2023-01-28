a6127b44700b984f1f1fe94e0ab71a802ca8d514
Dokaaa <122877634+Dokaaa@users.noreply.github.com>
2023-01-29 03:07:12 +0600

    Add files via upload

---
+++ week2/arrays/remove.py
@@ -0,0 +1,6 @@
+# pop() method to remove an element from the array
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.pop(1)
+
+print(cars)
---
+++ week2/arrays/remove2.py
@@ -0,0 +1,6 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.remove("Volvo")
+
+print(cars)
+# The list's remove() method only removes the first occurrence of the specified value
